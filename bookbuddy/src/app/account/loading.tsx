534135067c1ba3bcf41cb16db0bb6a83297a2c47
Norman7781 <aungkgmyat1129@gmail.com>
2025-10-05 19:09:12 +0700

    Add loading.tsx for account page to fix build error

---
+++ bookbuddy/src/app/account/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <img
+        src="/Figure-Gif-unscreen.gif"
+        alt="Loading"
+        className="w-24 h-24 object-contain"
+      />
+    </div>
+  );
+}
